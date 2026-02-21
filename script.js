0f5ccfb40f19bc078a0a024d3d1059ddfc9d2478
Otaviofertig <otavio.lesss@gmail.com>
2026-02-21 11:14:16 -0300

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+class Carro {
+    constructor(marca,fabricante) {
+        this.marca = marca;
+    }
+
+    mostrarMarca() {
+        console.log("a maarca do carro é $(this.marca)");
+    }
+}
+
+const meuCarro = new Carro("Ford");
+meuCarro.mostrarMarca();
